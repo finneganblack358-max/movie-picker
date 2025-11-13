a8837337d17abfe5dc727a3e77cfef5e949ce2cd
finneganblack358-max <finneganblack358@gmail.com>
2025-11-12 20:26:35 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from api.routes import api
+from api.models import db
+from api.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    app.register_blueprint(api)
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
