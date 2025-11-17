522ca38d8bfe9c8b8f84ae8e181e214d3ea45d86
finneganblack358-max <finneganblack358@gmail.com>
2025-11-16 21:34:32 -0500

    Trending Setup

--- api/config.py
+++ api/config.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TMDB_API_KEY = os.getenv("TMDB_API_KEY")
     
--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, render_template, request
+from flask import Blueprint, jsonify, render_template, current_app, request
+import requests
 
 api = Blueprint('api', __name__)
 
@@ -11,5 +12,16 @@ def genres():
     return render_template("genres.html")
 
 @api.route("/trending")
-def trending():
-    return render_template("trending.html")
+def trending_movies():
+    api_key = current_app.config["TMDB_API_KEY"]
+    url = f"https://api.themoviedb.org/3/trending/movie/week?api_key={api_key}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        movies = data.get("results", [])
+        
+        return render_template("trending.html", movies=movies)
+    else:
+        return f"Error fetching trending movies: {response.status_code}", response.status_code
